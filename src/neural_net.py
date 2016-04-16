7106ed5b3f3e367a39fad5ccb675141acf1265b5
Sharath Rao <sharathrao13@gmail.com>
2016-04-16 13:06:39 -0700

    Remove abs

--- src/neural_net.py
+++ src/neural_net.py
@@ -104,6 +104,7 @@ class TwoLayerNet(object):
         #############################################################################
 
         diff = (scores.transpose() - y).transpose()
+        print diff
         delta_output = self.replace_zero_with_small_value(np.absolute(diff))
 
         data_loss = np.sum(-np.log(delta_output))
@@ -238,10 +239,10 @@ class TwoLayerNet(object):
             W2 = self.params['W2']
             b2 = self.params['b2']
 
-            W1+= -learning_rate*np.absolute(update_to_W1)
-            W2+= -learning_rate*np.absolute(update_to_W2)
-            b1+= -learning_rate*np.absolute(update_to_b1)
-            b2+= -learning_rate*np.absolute(update_to_b2)
+            W1+= -learning_rate*(update_to_W1)
+            W2+= -learning_rate*(update_to_W2)
+            b1+= -learning_rate*(update_to_b1)
+            b2+= -learning_rate*(update_to_b2)
 
             print "Printing the min of weights and biases "
             print np.amin(W1)
