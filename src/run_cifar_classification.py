65c9c50afcaccb8bf8f49a2affa5fbf48ab54abb
Sharath Rao <sharathrao13@gmail.com>
2016-04-17 09:19:03 -0700

    divide the values

--- src/run_cifar_classification.py
+++ src/run_cifar_classification.py
@@ -2,7 +2,7 @@ import neural_net, data_utils
 import numpy as np
 
 if __name__ == '__main__':
-    X_train, y_train, X_test, y_test = data_utils.load_CIFAR10("C:\Users\SHARATH\Git\cs291k-mp1\dataset")
+    X_train, y_train, X_test, y_test = data_utils.load_CIFAR10("/cs/student/sharathrao/cs291k-mp1/dataset")
     nn = neural_net.TwoLayerNet(3072, 4000, 10, 0.0001)
 
     # Find val set
@@ -11,13 +11,13 @@ if __name__ == '__main__':
     learning_rate = 0.0000001
     learning_rate_decay = 0.50
     reg = 0.0000001
-    num_iters = 2
-    batch_size = 10000
+    num_iters = 5
+    batch_size = 1000
     verbose = True
 
     print "Normalizing Image "
-    X_train = X_train.astype(float)
-    X_test = X_test.astype(float)
+    X_train = X_train.astype(float)/255.0
+    X_test = X_test.astype(float)/255.0
     mean_image_value_train = np.mean(X_train, axis=0)
     X_train -= mean_image_value_train
     mean_image_value_test = np.mean(X_test, axis=0)
